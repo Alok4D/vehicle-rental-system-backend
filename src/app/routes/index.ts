3ffc655eadec2eb626cda39e9e4084083a1838f2
Alok Roy <alokroy602701@gmail.com>
2026-08-10 17:10:14 +0600

    fix: added missing module routes to index.ts

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -2,8 +2,28 @@ import { Router } from 'express';
 
 const router = Router();
 
+import { AuthRoutes } from '../modules/auth/auth.route';
+import { UserRoutes } from '../modules/user/user.route';
+import { VehicleRoutes } from '../modules/vehicle/vehicle.route';
+import { BookingRoutes } from '../modules/booking/booking.route';
+
 const moduleRoutes: any[] = [
-  // To be populated later
+  {
+    path: '/auth',
+    route: AuthRoutes,
+  },
+  {
+    path: '/users',
+    route: UserRoutes,
+  },
+  {
+    path: '/vehicles',
+    route: VehicleRoutes,
+  },
+  {
+    path: '/bookings',
+    route: BookingRoutes,
+  },
 ];
 
 moduleRoutes.forEach((route) => router.use(route.path, route.route));
